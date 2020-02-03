70b2f0f5ca73e4dba17076fd077e15cc95d549b5
Kang Jie Tan <kangjietan@gmail.com>
2020-02-02 17:25:57 -0800

    Add PropTypes to child components

--- react-client/src/components/VolumeBar.jsx
+++ react-client/src/components/VolumeBar.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const VolumeBar = ({
-  leave, val, change, visible, song
+  leave, val, change, visible, song,
 }) => (
   <div style={{ visibility: visible }} className="volume-slider-container" onMouseLeave={leave}>
     <input type="range" min="0" max="100" value={val} id="vol" name="volume" onChange={(e) => { change(e, song); }} />
   </div>
 );
+
+VolumeBar.propTypes = {
+  leave: PropTypes.func.isRequired,
+  val: PropTypes.number.isRequired,
+  change: PropTypes.func.isRequired,
+  visible: PropTypes.string.isRequired,
+  song: PropTypes.instanceOf(Element).isRequired,
+};
 
 export default VolumeBar;
